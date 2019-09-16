e89d5eb3cea3000bf42ec2f556f89e7489f7dd0d
faithkane3 <faith.x.kane@gmail.com>
2019-09-15 20:19:27 -0500

    Commit the two pieces of code used for mock_assessment.

--- mock_assessment.py
+++ mock_assessment.py
@@ -4,6 +4,9 @@ def normalize_name(word):
     return word.strip("0123456789!@#$%^&*_() +=\/?<>,.`~;:").lower().replace(" ","_")
 
 def cumsum(lst):
+    """cumsum takes in a list and returns a list integers from the first list
+    each added to the number before it
+    """
     for i in range(1,len(lst)):
         lst[i] = lst[i- 1 ] + lst[i]
     return lst
