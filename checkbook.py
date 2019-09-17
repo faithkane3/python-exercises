ca72fd50d64d8cb49bc73921548c512a20e7d9f9
faithkane3 <faith.x.kane@gmail.com>
2019-09-17 16:24:56 -0500

    Commit working checkbook app.

--- checkbook.py
+++ checkbook.py
@@ -1,5 +1,8 @@
+from time import sleep as wait
+
 print("\n--- Welcome to your terminal checkbook. ---\n\n")
 while True:
+    wait(.5)
     print(
     "What would you like to do today?\n\n"
     "1. view current balance\n"
@@ -8,27 +11,78 @@ while True:
     "4. exit\n"
     )
     choice = input("Your choice: \n")
+    # if user doesn't choose 1, 2, 3, or 4 they will be prompted again for valid input
     while choice not in "1234":
         print("That is not a valid option.\n")
         choice = input("Your choice: \n")
         continue
+    
+    
+    
+    
+    # choice 1 will print out current balance and return to prompt for 1, 2, 3, or 4
     if choice == "1":
-        print(f"Your current balance is {balance}.\n")
+        wait(1)
+        r =  open("checkbook_info.txt", "r")
+        print(f"Your balance is ${r.readline()}")
+        r.close()
+        wait(1)
+        # print(f"Your current balance is ${balance}.\n")
+        # wait(1)
         continue
+
+    # choice 2 will subtract a debit from the balance
     elif choice == "2":
         try:
+            wait(1)
             debit = float(input("How much would you like to withdrawal? \n"))
-            continue
-        except:
+            if debit > 0:
+                with open("checkbook_info.txt", "r") as r:
+                    balance = r.readline()
+                    fbalance = float(balance)
+                    fbalance -= debit
+                with open("checkbook_info.txt", "w") as w:
+                    w.write(str(fbalance))
+        except(ValueError):
+            wait(1)
             print("That is not a valid option.\n")
             continue
+    # choice 3 will add a deposit to the balance
+    # choice 2 will subtract a debit from the balance
     elif choice == "3":
         try:
+            wait(1)
             deposit = float(input("How much would you like to deposit? \n"))
-            continue
-        except:
+            if deposit > 0:
+                with open("checkbook_info.txt", "r") as r:
+                    balance = r.readline()
+                    fbalance = float(balance)
+                    fbalance += deposit
+                with open("checkbook_info.txt", "w") as w:
+                    w.write(str(fbalance))
+        except(ValueError):
+            wait(1)
             print("That is not a valid option.\n")
             continue
+    # choice 4 will thank the user and exit while True
     elif choice == "4":
         print("\nThanks! Have a great day.")
         break
+            
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
