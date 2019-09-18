7deccc26ab6e8fc49e6ba23455072acbf6d08c5d
faithkane3 <faith.x.kane@gmail.com>
2019-09-18 12:13:46 -0500

    Commit incomplete idea file.

--- checkbook_funcs.py
+++ checkbook_funcs.py
@@ -1,13 +1,4 @@
-# from random import randint
-# balance = random.randint(100, 5000)
 
-#balance = 500
-# print(balance)
 
-def debit(num):
-    balance -= num
-    return balance
-
-def deposit(num):
-    balance += num
-    return balance
+def reader():
+    
